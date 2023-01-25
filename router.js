60d251bba5ac79db24efae31f0047b92330db75c
DESKTOP-SHB77O2\phgst <phgst12@gmail.com>
2023-01-25 11:01:00 +0900

    save interest in local & change class by click event

--- router.js
+++ router.js
@@ -10,7 +10,73 @@ export default async function createRouter() {
     },
     interest: {
       page: "/pages/interest.html",
-      eventListener: () => {},
+      eventListener: () => {
+        const INTEREST_KEY = "interest";
+        let interestArr = [];
+        function saveInterest() {
+          localStorage.setItem(INTEREST_KEY, JSON.stringify(interestArr));
+        }
+        const savedInt = localStorage.getItem(INTEREST_KEY);
+        const parsedInt = JSON.parse(savedInt);
+        if (parsedInt !== null) {
+          interestArr = parsedInt;
+        }
+
+        const setIntClass = () =>
+          interestArr.forEach((interest) => {
+            if (interest.isSelected === true) {
+              const id = interest.id;
+              const selectedInt = document.querySelectorAll(`#${id}`);
+
+              selectedInt.forEach((element) =>
+                element.classList.add("selected", "chosen")
+              );
+            }
+          });
+
+        const interest = document.querySelectorAll(".interest_btn");
+
+        const handleInterest = (event) => {
+          const className = ["chosen", "selected"];
+          const id = event.id;
+
+          let isChosen;
+          interestArr.forEach((interest) => {
+            if (interest.id === id) {
+              interest.isSelected === true
+                ? (isChosen = true)
+                : (isChosen = false);
+            }
+          });
+          console.log("is?", isChosen);
+          if (isChosen) {
+            interestArr.forEach((interest) => {
+              interest.id === id ? (interest.isSelected = false) : "";
+              event.classList.remove(...className);
+            });
+          } else if (!isChosen) {
+            const newInterest = { id, isSelected: true };
+            interestArr.map((interest) => {
+              if (interest.id === id) {
+                interest.isSelected = true;
+              }
+            });
+            interestArr.push(newInterest);
+          }
+          interestArr = interestArr.reduce((acc, current) => {
+            if (acc.findIndex(({ id }) => id === current.id) === -1) {
+              acc.push(current);
+            }
+            return acc;
+          }, []);
+          saveInterest();
+          setIntClass();
+        };
+        interest.forEach((e) =>
+          e.addEventListener("click", () => handleInterest(e))
+        );
+        setIntClass();
+      },
     },
     feed: {
       page: "/pages/feed.html",
@@ -18,12 +84,13 @@ export default async function createRouter() {
         const cardWrapper = document.querySelector(".content_wrapper");
         getContents();
         const contentsArr = await getContents();
+        console.log(contentsArr);
 
         //card 배치 랜덤
         const contents = await contentsArr.popularity.sort(
           () => Math.random() - 0.5
         );
-        console.log(contents);
+
         const firstCard = `<div class="wrapper_block">
     ${contents // 추후 hash 로 대체하여 코드 재활용
       .slice(0, 18)
