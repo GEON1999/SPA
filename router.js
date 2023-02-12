62041e0c0826878f91ace5d20e719fe79ac282ed
DESKTOP-SHB77O2\phgst <phgst12@gmail.com>
2023-02-12 18:54:45 +0900

    bookmark & interest page set up

--- router.js
+++ router.js
@@ -1,48 +1,121 @@
 import { getContents } from "./util.js";
 
-export default function createRouter() {
+export default async function createRouter() {
+  const hash = window.location.hash.replace("#", "");
+  // pathName 잘 출력됨
+
   const routes = {
-    "": {
+    bookmark: {
+      page: "/pages/bookmark.html",
+    },
+    interest: {
       page: "/pages/interest.html",
       eventListener: () => {},
     },
     feed: {
       page: "/pages/feed.html",
       eventListener: async () => {
-        await getContents();
-        const contents = await getContents();
-        console.log(contents.investment);
-        const cardHtml = contents.investment
-          .map((item) => {
-            return `
-            <section class="news_single_item_small">
+        const cardWrapper = document.querySelector(".content_wrapper");
+        getContents();
+        const contentsArr = await getContents();
+
+        //card 배치 랜덤
+        const contents = await contentsArr.popularity.sort(
+          () => Math.random() - 0.5
+        );
+        console.log(contents);
+        const firstCard = `<div class="wrapper_block">
+    ${contents // 추후 hash 로 대체하여 코드 재활용
+      .slice(0, 18)
+      .map((item, i) => {
+        return ` <section class="${
+          i === 10 || i === 17 ? "news_single_item" : "news_single_item_small"
+        }">
+      <div>
+      <a href=${item.link} target="_blank">
+        <div class="thumb">
+          <img src=${item.thumbnail} />
+        </div>
+        <ul class="info">
+          <li>${item.origin}</li>
+        </ul>
+        <div class="title">${item.title}</div>
+      </a>
+    </div>
+    <div class="item_btns item_bottom">
+      <div><button type="button" class="btn_bookmark"></button></div>
+      <div>
+        <button type="button" class="btn_bookmark clicked"></button>
+      </div>
+    </div>
+    <div class="edit_bg"></div>
+  </section>`;
+      })
+      .join("")}</div>`;
+        cardWrapper.innerHTML = firstCard;
+
+        let index = 0;
+        let index2 = 18;
+        let isLarge = true;
+        // index = index2
+        // isLarge 가 false 면 index2 += 8 이되고 isLarge 가 true 됨
+        // index2 < contents.patName.length 까지 반복
+
+        // scroll event
+        document.addEventListener("scroll", () => {
+          if (
+            window.innerHeight + window.scrollY >=
+            document.body.scrollHeight
+          ) {
+            if (index2 < contents.length) {
+              index = index2;
+              console.log("index", index);
+              if (isLarge === true) {
+                index2 += 10;
+                isLarge = false;
+                console.log("isLarge", index2, isLarge);
+              } else if (isLarge === false) {
+                index2 += 8;
+                isLarge = true;
+                console.log("!isLarge", index2, isLarge);
+              }
+              const appendCard = `<div class="wrapper_block">
+          ${contents // 추후 hash 로 대체하여 코드 재활용
+            .slice(index, index2)
+            .map((item, i) => {
+              return ` <section class="${
+                (isLarge && i === 0) || (isLarge && i === 7)
+                  ? "news_single_item"
+                  : "news_single_item_small"
+              }">
             <div>
-            <a href=${item.link}>
+            <a href=${item.link} target="_blank">
               <div class="thumb">
                 <img src=${item.thumbnail} />
               </div>
               <ul class="info">
-                <li>${origin}</li>
+                <li>${item.origin}</li>
               </ul>
               <div class="title">${item.title}</div>
             </a>
-          </div>
-  
-          <div class="item_btns item_bottom">
-            <div><button type="button" class="btn_bookmark"></button></div>
-            <div>
-              <button type="button" class="btn_bookmark clicked"></button>
             </div>
-          </div>
-  
-          <div class="edit_bg"></div>
-          </section>
-          `;
-          })
-          .join("");
-
-        const card = document.querySelector(".wrapper_block");
-        card.innerHTML = cardHtml;
+            <div class="item_btns item_bottom">
+              <div><button type="button" class="btn_bookmark"></button></div>
+              <div>
+                <button type="button" class="btn_bookmark clicked"></button>
+              </div>
+            </div>
+            <div class="edit_bg"></div>
+          </section>`;
+            })
+            .join("")}</div>`;
+              const box = cardWrapper.appendChild(
+                document.createElement("div")
+              );
+              box.innerHTML = appendCard;
+            }
+          }
+        });
       },
     },
   };
